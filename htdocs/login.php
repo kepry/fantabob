1d0d85638a7f8d57f40d85b243da53a390e22ab4
kepry <kepridesousa@gmail.com>
2019-06-04 10:54:54 -0300

    update login.php
    fazendo comparação de dados antes de logar

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,30 +1,55 @@
-<?php
+   <?php
 session_start();
-require "conexão.php";
+include ("conexão.php");
 if (isset($_POST['email']) && empty($_POST['email']) == false) 
 	if (isset($_POST['senha']) && empty($_POST['senha']) == false) {
 		$email = addslashes($_POST['email']);
 		$senha = addslashes($_POST['senha']);
 
+
 		
 
 		try {
 			
-			$db = new PDO($dsn, $dbuser,$dbpass);
+			$db = new PDO($dsn, $dbuser, $dbpass);
+
 			$sql = $db->query("SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'");
 
 		if ($sql->rowCount() > 0) {
 			$dado = $sql->fetch();
-			$_SESSION['id'] = $dado['id'];
-			header("Location: index.php");
+			$senhaCompare = $dado['senha'];
+			$emailCompare = $dado['email'];
+
+			//recepção de dados 
+
+			if($emailCompare == $email && $senhaCompare === $senha){
+				$_SESSION['id'] = $dado['id'];
+				header("Location: ../index.php");
+				$log = "Logou".getMessage();
+			//método de comparaçãop de dados
 			}
-		} catch (PDOException $e) {
+		}
+			else{
+				header("location: ../login.html");
+				echo "Erro de Conexão".$e.getMessage();
+			
+		}
+
+			
+	} 	catch (PDOException $e) {
+				header("location: ../login.html");
 				echo "Erro de Conexão".$e.getMessage();
 		}
 
 
 }else{
-	echo 'Erro';
+	header("Location: ../login.html");
+}
+	
+
+else{
+	header("Location: ../login.html");
+	$erro = "Você não Entrou Com Todos os Dados!".getMessage();
 }
 
 ?>
